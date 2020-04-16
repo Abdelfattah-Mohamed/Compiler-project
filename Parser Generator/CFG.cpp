d1bcefba584445a8acc09dbe1c55109c00b21305
mohamedabdelhakem1 <mohamedabdelhakem99@yahoo.com>
2020-04-17 01:19:45 +0200

    before branching

--- Parser Generator/CFG.cpp
+++ Parser Generator/CFG.cpp
@@ -5,21 +5,23 @@ using namespace std;
 class CFG
 {
 private:
-    /// add helper functions here
+    /// add helper functions
+    string startSymbol;
     LexicalGeneratorBuilder *lexBuilder;
-    unordered_set<string> terminls;
+    unordered_set<string> terminals;
     unordered_map<string, vector<ProductionRule *>> nonTerminals;
     vector<ProductionRule *> productionRules;
     unordered_map<string, vector<string>> firstSets;
     unordered_map<string, vector<string>> FollowSets;
     unordered_map<string, unordered_map<string, ProductionRule>> ll1Table;
-    vector<string> sententialLines;
+    // non terminal , <ternminal , production >
+    vector<string> sententialLines; // result
 
 public:
-    CFG::CFG(LexicalGeneratorBuilder *_lex)
+    CFG(LexicalGeneratorBuilder *_lex)
     {
     }
-    /// set  DS of terminals and nonterminals and production rules
+    // set  DS of terminals and nonterminals and production rules
     void parseGrammerFile(string grammerFile)
     {
     }
